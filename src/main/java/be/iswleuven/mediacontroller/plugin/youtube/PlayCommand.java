2e97df0257db7d95c61f1f1ca7e6eb80c20f367a
Yuri Moens <yuri.moens@gmail.com>
2014-11-19 17:18:46 +0100

    Parse Youtube URLs.

--- src/main/java/be/iswleuven/mediacontroller/plugin/youtube/PlayCommand.java
+++ src/main/java/be/iswleuven/mediacontroller/plugin/youtube/PlayCommand.java
@@ -1,8 +1,15 @@
 package be.iswleuven.mediacontroller.plugin.youtube;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import be.iswleuven.mediacontroller.command.Command;
 import be.iswleuven.mediacontroller.command.CommandException;
 import be.iswleuven.mediacontroller.player.Playlist;
+import be.iswleuven.mediacontroller.player.Song;
 
 import com.google.inject.Inject;
 
@@ -31,7 +38,34 @@ public class PlayCommand extends Command {
 
   @Override
   public void execute() throws CommandException {
-    this.playlist.addSong(null);
+    String[] urls = getParameters();
+    
+    try {
+      Song[] songs = getSongs(urls);
+      setMessage(songs[0].toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    notifyWorker();
+  }
+  
+  private Song[] getSongs(String[] urls) throws IOException {
+    List<Song> songs = new ArrayList<Song>();
+    
+    for (String url : urls) {
+      Process p = Runtime.getRuntime().exec("python src/main/resources/youtube-dl --skip-download -g -e " + url);
+      
+      BufferedReader outputReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      
+      String title = outputReader.readLine();
+      String streamUrl = outputReader.readLine();
+      
+      Song song = new Song(title, streamUrl, getWorker().getAddress());
+      songs.add(song);
+    }
+    
+    return songs.toArray(new Song[songs.size()]);
   }
 
 }
